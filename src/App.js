e455ca7256550f8cb955965027b06ed17c9172cd
Sara Fritz <31226863+sarafritz88@users.noreply.github.com>
2018-03-30 13:23:11 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+
+import './App.css';
+
+import {
+    Container,
+    Row,
+    Col,
+
+} from 'reactstrap';
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <div>
+
+                <Container>
+                    <Row>
+                        <Col>.col one column, one row</Col>
+                    </Row>
+                    <Row>
+                        <Col>.col</Col>
+
+                        <Col>.col three cols, one row</Col>
+                        <Col>.col</Col>
+                    </Row>
+                    <Row>
+                        <Col xs="3">.col-3 This column will not resize. No Matter how much I put in it, it will wrap</Col>
+                        <Col xs="auto">.col-auto - variable width content This will stretch to fit whatever you put in </Col>
+                        <Col xs="3">.col-3 I adjusted this colums height by adjusting the height of the row it is in. All of the cols in this row, will match that height</Col>
+                    </Row>
+
+                </Container>
+            </div>
+
+        );
+    }
+}
+
+
+
+
+export default App;
+
